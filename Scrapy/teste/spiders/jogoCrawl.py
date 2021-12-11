29edd0f5cd7b9e8ece12aeb1a06b30afccf1b6cc
Anderson-Makino <anddersonful@gmail.com>
2021-12-11 16:46:35 -0300

    Progress in jogoCrawl script

--- Scrapy/teste/spiders/jogoCrawl.py
+++ Scrapy/teste/spiders/jogoCrawl.py
@@ -17,14 +17,22 @@ class JogoCrawlSpider(CrawlSpider):
     def parse_item(self, response):
         yield {
             'titulo': response.css('.css-1p6kk8h::text').get('').strip(),
-            # 'desenvolvedora': response.xpath('//div[@class="dev_row"]/div[@class="summary"]/text()').get(),
-            # 'categoria':response.xpath('//div[@class="popular_tags"]/a/text()').get(),
-            #'descricao': response.xpath('//div[@class="game_description_snippet"]/text()').get().strip(),
-            'ano_publicacao': response.css('time::text').get('').strip(),
-            'img_src': response.xpath('//img[@class="css-5iu9l4"]/@src').get('').strip()
-            # 'cpu': response.css('.bb_ul > li:nth-child(2)::text').get(),
-            # 'so': response.css('.bb_ul > li:nth-child(3)::text').get(),
-            # 'memoria_ram': response.css('.bb_ul > li:nth-child(4)::text').get(),
-            # 'gpu': response.css('.bb_ul > li:nth-child(5)::text').get(),
-            # 'armazenamento': response.css('.bb_ul > li:nth-child(8)::text').get(),
+            'desenvolvedora': response.xpath('//div[@class="css-10mlqmn"][1]/div[@class="css-fxdlmq"]/div[@class="css-btns76"]/span/text()').get(),
+            'publicadora': response.xpath('//div[@class="css-10mlqmn"][2]/div[@class="css-fxdlmq"]/div[@class="css-btns76"]/span/text()').get(),
+            'ano_publicacao': response.xpath('//div[@class="css-10mlqmn"][3]/div[@class="css-fxdlmq"]/div[@class="css-btns76"]/span/time/text()').get(),
+            
+
+            #pegando apenas a 1° categoria
+            'categoria':response.xpath('//div[@class="css-1kg0r30"][1]/div[@class="css-encdnt"]/div[@class="css-1pj7rfu"][2]/ul[@class="css-vs1xw0"]/li[@class="css-t8k7"][1]/a[@class="css-1672chc"]/span[@class="css-z3vg5b"]/span/text()').get(),
+            'img_src': response.xpath('//div[@class="css-u0wq1j"]/div/img/@src').get(''),
+
+            # mto grande, mas funcionando
+            #'descricao': response.xpath('//div[@id="about-long-description"]/span[@class="css-z3vg5b"]/div[@class="css-1lwib6p"]/div[@class="css-1chn1kq"]/text()').get(),
+
+            #problema de ordem para cada jogo
+            'so': response.xpath('//div[@class="css-3rds8q"][1]/div[@class="css-2sc5lq"][1]/span[@class="css-z3vg5b"]/text()').get(),
+            'cpu': response.xpath('//div[@class="css-3rds8q"][2]/div[@class="css-2sc5lq"][1]/span[@class="css-z3vg5b"]/text()').get(),
+            'memoria_ram': response.xpath('//div[@class="css-3rds8q"][3]/div[@class="css-2sc5lq"][1]/span[@class="css-z3vg5b"]/text()').get(),
+            'armazenamento': response.xpath('//div[@class="css-3rds8q"][4]/div[@class="css-2sc5lq"][1]/span[@class="css-z3vg5b"]/text()').get(),
+            'gpu': response.xpath('//div[@class="css-3rds8q"][6]/div[@class="css-2sc5lq"][1]/span[@class="css-z3vg5b"]/text()').get()
         }
