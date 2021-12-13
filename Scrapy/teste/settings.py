d83f68e6990ce5af51e226721b222678b29a6fc0
PedroPHS <40281726+PedroAugustoPHS@users.noreply.github.com>
2021-12-13 11:21:50 -0300

    Add files via upload

--- Scrapy/teste/settings.py
+++ Scrapy/teste/settings.py
@@ -19,6 +19,8 @@ NEWSPIDER_MODULE = 'teste.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+FEED_EXPORT_ENCODING = "utf-8"
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
