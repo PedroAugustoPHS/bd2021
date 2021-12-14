a9dde1ba5dedd853e92584491e28a6d80d1d5bdb
Anderson-Makino <anddersonful@gmail.com>
2021-12-13 21:29:25 -0300

    add nuuvem script

--- Scrapy/teste/spiders/nuuvem.py
+++ Scrapy/teste/spiders/nuuvem.py
@@ -5,7 +5,7 @@ from teste.prices import myPrice
 class NuuvemCrawlSpider(CrawlSpider):
     name = 'nuuvemCrawl'
     allowed_domains = ['www.nuuvem.com']
-    start_urls = ['https://www.nuuvem.com/catalog/platforms/pc/sort/title/sort-mode/asc']
+    start_urls = ['https://www.nuuvem.com/catalog/platforms/pc/sort/discount/sort-mode/desc']
 
     preco_nuuvem = LinkExtractor()
     rule_preco_nuuvem = Rule(preco_nuuvem, callback='parse_item', follow=False)
@@ -14,9 +14,13 @@ class NuuvemCrawlSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
+        real: str = response.xpath('//span[@class="product-price--val"]/span[@class="integer"]/text()').extract_first()
+        centavo: str = response.xpath('//span[@class="product-price--val"]/span[@class="decimal"]/text()').extract_first()
+        preco = (real + centavo)
         item = myPrice()
-        item['titulo'] = response.xpath('//h3[@class="product-title double-line-name"]')
-        item['preco'] = response.xpath('//span[@class="product-price--val"]')
-        item['desconto'] = response.xpath('//span[@class="product-price--discount"]')
+        item['titulo'] = response.xpath('//h1[@class="product-title"]/span/text()').extract_first() or response.xpath('//h3[@class="product-title single-line-name"]/text()').extract_first()
+        item['preco'] = preco
+        item['preco_sem_desconto'] = response.xpath('//span[@class="product-price--val"]/span[@class="product-price--old"]/text()').extract_first()
+        item['desconto'] = response.xpath('//span[@class="product-price--discount"]/text()').extract_first() or '0'
     
         yield item
