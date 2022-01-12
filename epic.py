2e11161f0f8eae1be44a017ae13c85e18ae2ab34
PedroPHS <40281726+PedroAugustoPHS@users.noreply.github.com>
2022-01-12 11:35:19 -0300

    Alterando os Scrapys
    Agora os crawlers serão rodados em páginas individuais

---
+++ epic.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy.spiders import Spider
+from teste.prices import myPrice
+class EpicCrawlSpider(Spider):
+    name = 'epicCrawl'
+    allowed_domains = ['epicgames.com']
+    start_urls = [
+        'https://www.epicgames.com/store/pt-BR/p/god-of-war',
+        'https://www.epicgames.com/store/pt-BR/p/naraka-bladepoint',
+    ]
+
+    def parse(self, response):
+        item = myPrice()
+        item['titulo'] =  response.css('.css-1p6kk8h::text').extract() or response.css('.css-j00jcq::text').extract()
+        item['preco'] = (response.css('.css-l24hbj > .css-z3vg5b::text').extract()
+            or response.xpath('//div[@class="css-l24hbj"][2]/span[@class="css-z3vg5b"]')
+            or response.css('.css-l24hbj > .css-z3vg5b > span::text').extract()
+            or response.css('.css-8en90x > span::text').extract()
+            or 'não especificado')
+        item['preco_sem_desconto'] = response.css('.css-11ksoqt > .css-1rcj98u::text').extract() or 'sem desconto'
+        item['desconto'] = response.css('.css-15fdr99 > div::text').get() or '0'
+        
+        yield item
